668ad337ed7136ff4e29e1a41e517926e806492e
VISHNUVARDHAN25012002 <116712471+VISHNUVARDHAN25012002@users.noreply.github.com>
2024-11-04 15:56:24 +0530

    minor change

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css"
 const Footer=()=>{
     return(
-        <footer className="footer">copyright &#169; 2023 vishnu vardhan.All right reserved.</footer>
+        <footer className="footer">copyright &#169; 2024 vishnu vardhan.All right reserved.</footer>
     )
 }
 export default Footer;
